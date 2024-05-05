854ce4a6c43117797e5bbd02b9307f7923769991
ashkaty <ashkaty@gmail.com>
2024-05-05 15:38:17 -0500

    added read.me

--- index.js
+++ index.js
@@ -5,44 +5,42 @@ async function run() {
 
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
-    const GRID_WIDTH = 226;
-    const GRID_HEIGHT = 126;
-    const CELL_SIZE = 5;
-    const TARGET_FPS = 60; // Desired frame rate
-    const FRAME_INTERVAL = 1000 / TARGET_FPS; // Time between frames in milliseconds
+    const gridWidth = 226;
+    const gridHeight = 126;
+    const cellSize = 5;
 
-    const grid = new Grid(GRID_WIDTH, GRID_HEIGHT);
 
-    canvas.width = GRID_WIDTH * CELL_SIZE;
-    canvas.height = GRID_HEIGHT * CELL_SIZE;
+    const grid = new Grid(gridWidth, gridHeight);
 
-    let lastUpdateTime = performance.now();
+    canvas.width = gridWidth * cellSize;
+    canvas.height = gridHeight * cellSize;
+
+    
 
     canvas.addEventListener('click', (event) => {
         const rect = canvas.getBoundingClientRect();
-        const x = Math.floor((event.clientX - rect.left) / CELL_SIZE);
-        const y = Math.floor((event.clientY - rect.top) / CELL_SIZE);
+        const x = Math.floor((event.clientX - rect.left) / cellSize);
+        const y = Math.floor((event.clientY - rect.top) / cellSize);
         grid.set(new Vector2(x, y), Element.sand());
     });
 
     function update() {
-        const currentTime = performance.now();
-        const elapsed = currentTime - lastUpdateTime;
 
-        if (elapsed >= FRAME_INTERVAL) {
-            grid.update();
-            lastUpdateTime = currentTime;
-        }
+
+        grid.update();
+
+    
 
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        for (let y = 0; y < GRID_HEIGHT; y++) {
-            for (let x = 0; x < GRID_WIDTH; x++) {
+        for (let y = 0; y < gridHeight; y++) {
+            for (let x = 0; x < gridWidth; x++) {
                 const element = grid.get(new Vector2(x, y));
+                // console.log(`${element.to_string()}`)
                 const color = element.get_color();
                 ctx.fillStyle = `rgb(${color.r}, ${color.g}, ${color.b})`;
-                ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
             }
         }
 
